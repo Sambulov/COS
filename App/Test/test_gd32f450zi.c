9e9e6de8990d2a36750ceff8d28d559a5bd8791b
a.bodin <Bodin98@mail.ru>
2024-04-25 17:40:56 +0300

    uncommented vector table

--- App/Test/test_gd32f450zi.c
+++ App/Test/test_gd32f450zi.c
@@ -34,11 +34,11 @@
     .priority_group = 1,
   };
 
-  hdl_nvic_exti_t mod_nvic_exti_line_14 = {
-    .line = HDL_EXTI_LINE_14,
+  hdl_nvic_exti_t mod_nvic_exti_line_13 = {
+    .line = HDL_EXTI_LINE_13,
     .mode = HDL_EXTI_MODE_INTERRUPT,
     .source = HDL_EXTI_SOURCE_PE,
-    .trigger = HDL_EXTI_TRIGGER_FALLING
+    .trigger = HDL_EXTI_TRIGGER_RISING
   };
 
   hdl_nvic_t mod_nvic = {
@@ -48,7 +48,7 @@
     .prio_bits = HDL_INTERRUPT_PRIO_GROUP_BITS,
     .irq_latency = 0, /* TODO: define static assert */
     .interrupts = hdl_interrupts(&mod_irq_systick, &mod_irq_exti_10_15),
-    .exti_lines = hdl_exti_lines(&mod_nvic_exti_line_14)
+    .exti_lines = hdl_exti_lines(&mod_nvic_exti_line_13)
   };
 
 hdl_gpio_port_t hdl_gpio_port_e = {
@@ -62,38 +62,37 @@ hdl_gpio_mode_t gpio_input_np = {
     .pull = GPIO_PUPD_NONE,
 };
 
-hdl_gpio_pin_t pin_pe14 = {
+hdl_gpio_pin_t pin_pe13 = {
     .module.init = &hdl_gpio_pin,
     .module.dependencies = hdl_module_dependencies(&hdl_gpio_port_e),
-    .module.reg = (void *)GPIO_PIN_14,
+    .module.reg = (void *)GPIO_PIN_13,
     .mode = &gpio_input_np
 };
 
 
 void test() {
-    SystemInit();
+    //SystemInit();
 
     hdl_enable(&mod_sys_core.module);
     hdl_enable(&mod_nvic.module);
-    hdl_enable(&pin_pe1.module);
+    hdl_enable(&pin_pe13.module);
 
   while (!hdl_init_complete()) {
     cooperative_scheduler(false);
   }
   
-  SysTick_Config(200000);
-
-  rcu_periph_clock_enable(RCU_GPIOE);
-  gpio_af_set(GPIOE, GPIO_AF_0, GPIO_PIN_13);
-  gpio_mode_set(GPIOE, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO_PIN_13);
+  //SysTick_Config(200000);
 
 
-  hdl_interrupt_request(&mod_nvic, HDL_NVIC_EXCEPTION_SysTick, &SysTick_Event, NULL);
+
+
+  //hdl_interrupt_request(&mod_nvic, HDL_NVIC_EXCEPTION_SysTick, &SysTick_Event, NULL);
   hdl_interrupt_request(&mod_nvic, HDL_NVIC_IRQ40_EXTI10_15, &Exti_Event, NULL);
+  //hdl_exti_sw_trigger(&mod_nvic, HDL_NVIC_IRQ40_EXTI10_15);
   while (1)
   {
-    // if(hdl_gpio_read(&pin_pe13) == HDL_GPIO_LOW)
-    //   __NOP();
+    if(hdl_gpio_read(&pin_pe13) == HDL_GPIO_LOW)
+       __NOP();
 
     cooperative_scheduler(false);
   }

70ca4dd0b19893f469043a1e31cda57453e84ba1
a.bodin <Bodin98@mail.ru>
2024-05-16 17:10:01 +0300

    second_relize

--- App/Protocol/Register_protocol/Inc/atb3500_reg_protocol.h
+++ App/Protocol/Register_protocol/Inc/atb3500_reg_protocol.h
@@ -3,6 +3,17 @@
 
 #include "cl_transceiver_reg_protocol.h"
 
+/* Protocol */
+/*
+* Byte amount [ 1 ] [   1   ] [  1  ] [   4    ]
+* Read 4 byte [CMD] [ADDRESS] [DUMMY] [REGISTER] [STOP CONDITIONAL]
+*/
+
+#define REG_PROTOCOL_AMOUNT_ADDRESS_BYTE            ((int16_t) 1)
+#define REG_PROTOCOL_AMOUNT_REGISTER_BYTE           ((int16_t) 4)
+
+
+
 /* Register mood */
 typedef enum {
     REG_PROTOCOL_ATB3500_CMD_READ_4_BYTE = (uint8_t)1,
@@ -14,13 +25,30 @@ typedef enum {
     REG_PROTOCOL_ATB3500_WRITE_REG,
 } reg_protocol_atb3500_read_write_reg_e;
 
-typedef struct {
-    uint8_t reg_address; /* buffer for saving register adrress */
-    uint8_t cnt;         /* buffer for saving byte transfer */
-    uint32_t new_value;  /* buffer for saving new reg value */
-    void (*write_read_reg)(uint32_t *data, uint8_t address, reg_protocol_atb3500_read_write_reg_e option);
-} reg_protocol_atb3500_read_write_reg_contex_t;
+typedef enum {
+    REG_PROTOCOL_SM_INITIAL,
+    REG_PROTOCOL_SM_WAITING_ADDRESS,
+    REG_PROTOCOL_SM_WAITING_REGISTER,
+    REG_PROTOCOL_SM_WRITE_REQUEST,
+    REG_PROTOCOL_SM_COMPLETED,
+    REG_PROTOCOL_SM_ERROR,
+} reg_protocol_state_machine_e;
 
+typedef struct {
+    reg_protocol_state_machine_e state_machine;
+    uint32_t reg_address; /* buffer for saving register adrress */
+    int16_t cnt_addres;     /* buffer for saving rx bytes counter */
+    uint32_t reg_value;  /* buffer for saving new reg value */
+    int32_t (*read_reg)(uint32_t *data, uint8_t address, reg_protocol_atb3500_read_write_reg_e option);
+} reg_protocol_atb3500_read_reg_contex_t;
+
+typedef struct {
+    reg_protocol_state_machine_e state_machine;
+    uint8_t reg_address; /* buffer for saving register adrress */
+    uint8_t cnt; /* buffer for saving byte transfer */
+    uint32_t reg_value;  /* buffer for saving new reg value */
+    int32_t (*write_reg)(uint32_t *data, uint8_t address, reg_protocol_atb3500_read_write_reg_e option);
+} reg_protocol_atb3500_write_reg_contex_t;
 
 
 #endif
--- CL/Transceiver/Transceiver_reg_protocol/Src/cl_transceiver_reg_protocol.c
+++ CL/Transceiver/Transceiver_reg_protocol/Src/cl_transceiver_reg_protocol.c
@@ -1,5 +1,15 @@
 #include "cl_transceiver_reg_protocol.h"
 
+/* Write register 1 */
+#define TEST_1 
+/* Unsuppoted cmd */ 
+//#define TEST_2 
+/* Unsuppoted reg address */ 
+//#define TEST_3
+/* Read register 1 */
+//#define TEST_4 
+
+
 cl_reg_protocol_command_t *_reg_protocol_find_command(cl_reg_protocol_transceiver_h *h, uint8_t command) {
     cl_reg_protocol_command_t **command_array = h->cmd_array;
     cl_reg_protocol_command_t *rez = NULL;
@@ -9,63 +19,140 @@ cl_reg_protocol_command_t *_reg_protocol_find_command(cl_reg_protocol_transceive
             rez = (*command_array);
             break;
         }
+        command_array++;
     }
     return rez;
 }
 
-void cl_protocol_transceiver_worker(cl_reg_protocol_transceiver_h *h) {
+int32_t cl_protocol_reg_end_of_transaction_callback(void *context_base) {
+    cl_reg_protocol_transceiver_h *h = (cl_reg_protocol_transceiver_h *)context_base;
     cl_reg_protocol_command_t *command = NULL;
-    uint8_t new_value_from_interface;
-    uint8_t end_of_transaction = 0;
-    /* If we have`t goten new data, return */
-    if(h->transceiver_handler->pfEndOfTransactionCallback(&h->transceiver_handler->pxProtoDescriptor) == 0)
-        end_of_transaction = 1;
-    else if (h->transceiver_handler->pfRxDataCallback(&h->transceiver_handler->pxProtoDescriptor, &new_value_from_interface, 1) == 0)
-        return;
-        
-    if(h->state_machine == CL_REG_PROTOCOL_SM_ERROR)
-        h->state_machine = CL_REG_PROTOCOL_SM_WAITING_CMD;
-    switch (h->state_machine)
-    {
-        case CL_REG_PROTOCOL_SM_WAITING_CMD: {
-            command = _reg_protocol_find_command(h, new_value_from_interface);
-            if(command != NULL) {
-                h->state_machine = CL_REG_PROTOCOL_SM_COMMAND_PROC;
-                h->current_cmd = new_value_from_interface;
-            }
-        }
-            break;
+    switch (h->state_machine) {
         case CL_REG_PROTOCOL_SM_COMMAND_PROC: {
             command = _reg_protocol_find_command(h, h->current_cmd);
-            if(end_of_transaction) {
-                /* Call user function */
-                command->end_of_transaction_callback(&command->context);
-                h->current_cmd = 0;
-                h->state_machine = CL_REG_PROTOCOL_SM_WAITING_CMD;
-                break;
-            }         
+            command->context_base.state = CL_REG_CONTEXT_SM_COMPLETED;
+            /* Call user function */
+            command->end_of_transaction_callback(&command->context_base);
+        }
+            break;
+        default:
+            break;
+    }
+    h->current_cmd = 0;
+    h->state_machine = CL_REG_PROTOCOL_SM_WAITING_CMD;
+    return 0;
+}
+
+int32_t cl_protocol_reg_tx_data_callback(void *context_base, uint8_t *data, uint16_t count) {
+    cl_reg_protocol_transceiver_h *h = (cl_reg_protocol_transceiver_h *)context_base;
+    cl_reg_protocol_command_t *command = NULL;
+    //data = NULL;
+    switch (h->state_machine) {
+        case CL_REG_PROTOCOL_SM_COMMAND_PROC: {
+            command = _reg_protocol_find_command(h, h->current_cmd);
             /* Check current state machine in the context */
-            if(command->context.state == CL_REG_PROTOCOL_CONTEXT_SM_WAITING_ADDRESS ||
-                command->context.state == CL_REG_PROTOCOL_CONTEXT_SM_PROC) 
-            {
+            if(command->context_base.state == CL_REG_CONTEXT_SM_PROC) {
                 /* Call user function trnasfer */
-                command->rx_data_callback(&command->context, &new_value_from_interface, 1, h->current_cmd);
-                /* Switch state machine, if transfer complete */
-                if(command->context.state == CL_REG_PROTOCOL_CONTEXT_SM_COMPLETED || 
-                        command->context.state == CL_REG_PROTOCOL_CONTEXT_SM_ERROR)
-                {
-                    h->current_cmd = 0;
-                    h->state_machine = CL_REG_PROTOCOL_SM_WAITING_CMD;
-                }
-            }
-            else {
-                h->current_cmd = 0;
-                h->state_machine = CL_REG_PROTOCOL_SM_WAITING_CMD;
+                 return command->tx_data_callback(&command->context_base, data, count);
             }
         }
             break;
-    default:
-        break;
+        default:
+            break;
     }
-    return;
+    return 0;
+}
+
+int32_t cl_protocol_reg_rx_data_callback(void *context_base, uint8_t *data, uint16_t count) {
+    cl_reg_protocol_transceiver_h *h = (cl_reg_protocol_transceiver_h *)context_base;
+    cl_reg_protocol_command_t *command = NULL;
+    int32_t number_of_obtained_bytes = 0;
+    while (count != 0) {
+        switch (h->state_machine)
+        {
+            case CL_REG_PROTOCOL_SM_WAITING_CMD:
+            case CL_REG_PROTOCOL_SM_ERROR: {
+                /* Find new command */
+                command = _reg_protocol_find_command(h, *data);
+                if(command != NULL) {
+                    h->state_machine = CL_REG_PROTOCOL_SM_COMMAND_PROC;
+                    h->current_cmd = *data;
+                    /* Switch context state */
+                    command->context_base.state = CL_REG_CONTEXT_SM_PROC;
+                    count--;
+                    number_of_obtained_bytes++;
+                }
+                else
+                    return 1;   /* First byte it is`t supported command */
+            }
+                break;
+            case CL_REG_PROTOCOL_SM_COMMAND_PROC: {
+                command = _reg_protocol_find_command(h, h->current_cmd);
+                if(command != NULL) {
+                    int32_t obtained_byte_by_command = command->rx_data_callback(&command->context_base, data, count);
+                    number_of_obtained_bytes += obtained_byte_by_command;
+                    return number_of_obtained_bytes;
+                }
+                else {
+                    h->state_machine = CL_REG_PROTOCOL_SM_ERROR;
+                    return number_of_obtained_bytes;
+                }
+            }
+                break;
+        default:
+            break;
+        }
+    }
+    return number_of_obtained_bytes;
+}
+
+void cl_protocol_transceiver_worker(cl_reg_protocol_transceiver_h *h) {
+#ifdef TEST_1
+    uint8_t input_data[] = {2, 1, 0xaa, 0, 0, 0};
+    static uint8_t cnt = 0;
+    uint8_t size = (sizeof(input_data)/sizeof(input_data[0])); 
+
+    if(cnt >= size)
+        cnt = 0;
+
+    h->transceiver_handler->pfRxDataCallback(h, &input_data[cnt], 1);
+    cnt++;
+#endif
+
+#ifdef TEST_2
+    uint8_t input_data[] = {3, 1, 0, 0, 0};
+    static uint8_t cnt = 0;
+
+    if(cnt > (sizeof(input_data)/sizeof(input_data[0])))
+        cnt = 0;
+
+    h->transceiver_handler->pfRxDataCallback(h, &input_data[cnt], 1);
+    cnt++;
+#endif
+
+#ifdef TEST_3
+    uint8_t input_data[] = {2, 0, 0, 0, 0};
+    static uint8_t cnt = 0;
+
+    if(cnt > (sizeof(input_data)/sizeof(input_data[0])))
+        cnt = 0;
+
+    h->transceiver_handler->pfRxDataCallback(h, &input_data[cnt], 1);
+    cnt++;
+#endif
+
+#ifdef TEST_4
+    uint8_t input_data[] = {1, 0, 0, 0, 1};
+    static uint8_t cnt = 0;
+
+    if(cnt > (sizeof(input_data)/sizeof(input_data[0])))
+        cnt = 0;
+    
+    if(cnt < 1)
+        h->transceiver_handler->pfRxDataCallback(h, &input_data[cnt], 1);
+    else
+        h->transceiver_handler->pfTxEmptyCallback(h, &input_data[cnt], 1);
+    cnt++;
+#endif
+    
 }

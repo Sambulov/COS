2465576490f97ef04645abba6996dd7fc411c809
Dmitry Sambulov <d.sambulov@atb-e.ru>
2024-10-17 14:35:30 +0300

    st & nu clock driver ref

--- HDL/McuPort/ARM/Gigadevice/GD32F450/Port/Inc/port_clock.h
+++ HDL/McuPort/ARM/Gigadevice/GD32F450/Port/Inc/port_clock.h
@@ -31,24 +31,4 @@ typedef struct {
   hdl_clock_property_t property;
 } hdl_clock_config_t;
 
-
-hdl_module_state_t hdl_clock_hxtal(void *desc, uint8_t enable);
-hdl_module_state_t hdl_clock_lxtal(void *desc, uint8_t enable);
-hdl_module_state_t hdl_clock_irc16m(void *desc, uint8_t enable);
-hdl_module_state_t hdl_clock_irc40k(void *desc, uint8_t enable);
-hdl_module_state_t hdl_clock_irc48m(void *desc, uint8_t enable);
-
-hdl_module_state_t hdl_clock_selector_pll(void *desc, uint8_t enable);
-
-hdl_module_state_t hdl_clock_selector_pll_vco(void *desc, uint8_t enable);
-hdl_module_state_t hdl_clock_pll_n(void *desc, uint8_t enable);
-hdl_module_state_t hdl_clock_pll_p(void *desc, uint8_t enable);
-hdl_module_state_t hdl_clock_pll_q(void *desc, uint8_t enable);
-
-hdl_module_state_t hdl_clock_system(void *desc, uint8_t enable);
-
-hdl_module_state_t hdl_clock_ahb(void *desc, uint8_t enable);
-hdl_module_state_t hdl_clock_apb1(void *desc, uint8_t enable);
-hdl_module_state_t hdl_clock_apb2(void *desc, uint8_t enable);
-
 #endif

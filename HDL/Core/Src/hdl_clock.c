94358acdafdfc3ea5c9217baf0dfcbc1af4e122f
Dmitry Sambulov <dmitry.sambulov@gmail.com>
2026-07-06 12:49:44 +0300

    decimal math

--- HDL/Core/Src/hdl_clock.c
+++ HDL/Core/Src/hdl_clock.c
@@ -1,17 +1,11 @@
 #include "hdl_iface.h"
 
 void hdl_clock_calc_mul(hdl_clock_freq_t *in, uint32_t factor, hdl_clock_freq_t *out) {
-  if((out != NULL) && (in != NULL)) {
-    out->denom = in->denom;
-    out->num = in->num;
-    decimal32_mull(out, factor);
-  }
+  if((out != NULL) && (in != NULL))
+    *out = decimal32_mul(*in, decimal32_from_int(factor));
 }
 
 void hdl_clock_calc_div(hdl_clock_freq_t *in, uint32_t factor, hdl_clock_freq_t *out) {
-  if((out != NULL) && (in != NULL)) {
-    out->denom = in->denom;
-    out->num = in->num;
-    decimal32_div(out, factor);
-  }
+  if((out != NULL) && (in != NULL))
+    *out = decimal32_div(*in, decimal32_from_int(factor));
 }
